bf82ded24be2204c75467204eca7a0cccb837f43
manu-sisto <manu-sisto@users.noreply.github.com>
2021-01-25 13:26:00 -0300

    dice que letra contiene la carta y si es primer o segundo click

--- index.js
+++ index.js
@@ -5,17 +5,31 @@ const letras = ["a", "b" ,"c", "d", "f", "g", "a", "b" ,"c", "d", "f", "g"];
 for (let i = 0; i < cartas.length; i++) {
     const element = cartas[i];
     element.setAttribute("data-carta", letras[i]);
-    element.addEventListener("click", function(){
-        alert("esta es la carta: " + element.getAttribute("data-carta"));
+    element.addEventListener("click", function () {
+        segundaCarta(element)
+        element.classList.add("activa")
+        var letra = element.getAttribute("data-carta")
+        manejarClickEnCarta(i, letra, element)
+        
     })
 };
 
-function darVueltaCarta(carta, primerCarta, segundaCarta){
-    if (carta === primerCarta) {
-        element.addEventListener("click", function(){
-            alert("esta es la carta: " + element.getAttribute("data-carta"));
-        })
+function segundaCarta (element) {
+    if (element.classList.contains("activa")) {
+        console.log("es segunda")
+    } else {
+        console.log("es primera")
     }
+}
+
+function manejarClickEnCarta(i, letra, element) {
+    console.log('click en carta', i, 'con letra', letra, element);
+};
+
+
+
+
+
     //hacer click en primera carta?
     //si o no?
     //si, entonces dar vuelta. y volver al principio de funcion (recursion)
@@ -25,7 +39,7 @@ function darVueltaCarta(carta, primerCarta, segundaCarta){
             //quedan mas cartas?
             //si, recursion.
             //no, ganaste!
-}
+
 
 
 
