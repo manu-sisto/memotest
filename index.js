e3868d5be6857ce4dbf820ebafe28bab26f40e92
Diego Sisto <diego.sisto@xapo.com>
2021-01-25 11:45:08 -0300

    Pequeño refactor y cambié alert por console.log

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ const letras = ["a", "b" ,"c", "d", "f", "g", "a", "b" ,"c", "d", "f", "g"];
 for (let i = 0; i < cartas.length; i++) {
     const element = cartas[i];
     element.setAttribute("data-carta", letras[i]);
-    element.addEventListener("click", function(){
-        alert("esta es la carta: " + element.getAttribute("data-carta"));
+    element.addEventListener("click", function() {
+        var data = element.getAttribute("data-carta")
+        manejarClickEnCarta(i, data)
     })
 };
 
+function manejarClickEnCarta(i, letra) {
+    console.log('Click en la carta', i, 'con letra', letra)
+}
 
 //document.querySelectorAll(".carta").forEach((carta) => carta.addEventListener('click', () => alert('me hicieron click')))
